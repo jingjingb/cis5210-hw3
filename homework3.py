b4b71f4d82c5dcaafb4d23b3910883cae180f6bc
jingjingb <baijingj@seas.upenn.edu>
2023-02-04 21:22:55 -0500

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -19,6 +19,195 @@ import math
 ############################################################
 # Section 1: Tile Puzzle
 ############################################################
+
+
+def create_tile_puzzle(rows, cols):
+    board = []
+    new = []
+    cnt = 1
+    for i in range(0, rows):
+        for j in range(0, cols):
+            new.append(cnt)
+            cnt += 1
+        board.append(new)
+        new = []
+    board[rows - 1][cols - 1] = 0
+
+    return TilePuzzle(board)
+
+
+class TilePuzzle(object):
+    
+    def __init__(self, board):
+        self.board = board
+        self.r = len(board)
+        self.c = len(board[0])
+        for i in range(self.r):
+            for j in range(self.c):
+                if board[i][j] == 0:
+                    self.loc = (i, j)
+        self.sol = self.solved_board()
+        self.h = 0
+        self.f = 0
+        self.g = 0
+        self.route = []
+
+    def get_board(self):
+        return self.board
+
+    def perform_move(self, direction):
+        loc = self.loc
+        if direction == "up":
+            if loc[0] == 0:
+                return False
+            else:
+                self.board[loc[0]][loc[1]] = self.board[loc[0] - 1][loc[1]]
+                self.board[loc[0] - 1][loc[1]] = 0
+                self.loc = (loc[0] - 1, loc[1])
+                return True
+        elif direction == "down":
+            if loc[0] == self.r - 1:
+                return False
+            else:
+                self.board[loc[0]][loc[1]] = self.board[loc[0] + 1][loc[1]]
+                self.board[loc[0] + 1][loc[1]] = 0
+                self.loc = (loc[0] + 1, loc[1])
+                return True
+        elif direction == "left":
+            if loc[1] == 0:
+                return False
+            else:
+                self.board[loc[0]][loc[1]] = self.board[loc[0]][loc[1] - 1]
+                self.board[loc[0]][loc[1] - 1] = 0
+                self.loc = (loc[0], loc[1] - 1)
+                return True
+        elif direction == "right":
+            if loc[1] == self.c - 1:
+                return False
+            else:
+                self.board[loc[0]][loc[1]] = self.board[loc[0]][loc[1] + 1]
+                self.board[loc[0]][loc[1] + 1] = 0
+                self.loc = (loc[0], loc[1] + 1)
+                return True
+        return False
+
+    def scramble(self, num_moves):
+        directions = ["up", "down", "left", "right"]
+        for i in range(num_moves):
+            self.perform_move(random.choice(directions))
+
+    def is_solved(self):
+        solved = create_tile_puzzle(self.r, self.c)
+        if self.board == solved.get_board():
+            return True
+        return False
+
+    def copy(self):
+        return TilePuzzle(copy.deepcopy(self.board))
+
+    def successors(self):
+        p = self.copy()
+        if p.perform_move("up"):
+            yield ("up", p)
+        p = self.copy()
+        if p.perform_move("down"):
+            yield ("down", p)
+        p = self.copy()
+        if p.perform_move("left"):
+            yield ("left", p)
+        p = self.copy()
+        if p.perform_move("right"):
+            yield ("right", p)
+
+    def solved_board(self):
+        board = []
+        new = []
+        cnt = 1
+        for i in range(0, self.r):
+            for j in range(0, self.c):
+                new.append(cnt)
+                cnt += 1
+            board.append(new)
+            new = []
+        board[self.r - 1][self.c - 1] = 0
+        return board
+
+    # Required
+    def find_solutions_iddfs(self):
+        is_found_solution = False
+        limit = 0
+        while not is_found_solution:
+            for move in self.iddfs_helper(limit, []):
+                yield move
+                is_found_solution = True
+            limit += 1
+
+    def iddfs_helper(self, limit, route):
+        if self.board == self.sol:
+            yield route
+        elif len(route) < limit:
+            for move, puzzle in self.successors():
+                for sol in puzzle.iddfs_helper(limit, route + [move]):
+                    yield sol
+
+    # Required
+    def find_solution_a_star(self):
+        open_set = set()
+        closed_set = set()
+        open_set.add(self)
+        self.h = self.manhattan(self.sol)
+        self.route = []
+
+        while open_set:
+            curr = min(open_set, key=lambda x: x.f)
+
+            if curr.board == self.sol:
+                return curr.route
+            open_set.remove(curr)
+
+            for move, puzzle in curr.successors():
+                if puzzle.board == self.sol:
+                    puzzle.route = curr.route + [move]
+                    return puzzle.route
+
+                puzzle.g = curr.g + curr.manhattan(puzzle.board)
+                puzzle.h = puzzle.manhattan(self.sol)
+                puzzle.f = puzzle.g + puzzle.h
+
+                go = True
+                for board in open_set:
+                    if board.board == puzzle.board and board.f < puzzle.f:
+                        go = False
+                        continue
+                for board in closed_set:
+                    if board.board == puzzle.board and board.f < puzzle.f:
+                        go = False
+                        continue
+                if go:
+                    open_set.add(puzzle)
+                    puzzle.route = curr.route + [move]
+
+            closed_set.add(curr)
+
+    def manhattan(self, t1):
+        total = 0
+        pos = {}
+
+        for x in range(self.r):
+            for y in range(self.c):
+                pos[t1[x][y]] = (x, y)
+
+        for x in range(self.r):
+            for y in range(self.c):
+                a = self.board[x][y]
+                pos2 = pos[a]
+                total += abs(x - pos2[0]) + abs(y - pos2[1])
+        return total
+
+
+           
+
+"""
 def create_tile_puzzle(rows, cols):
   return TilePuzzle([[row*cols + col + 1 if row*cols + col + 1<cols*rows else 0 for col in range(cols)] for row in range(rows)])
 
@@ -122,7 +311,7 @@ class TilePuzzle(object):
             for (move, new_p) in node[3].successors():
                 if tuple(tuple(x) for x in new_p.board) not in trace:
                     pq.put((node[1] + 1 + new_p.heuristic_md(), node[1] + 1, node[2] + [move], new_p))
-
+"""
 ############################################################
 # Section 2: Grid Navigation
 ############################################################
